a1ce5a196c6e6179639b4745297e905f8511a11d
Ayan <ayandgr5@gmail.com>
2023-11-14 14:40:14 +0530

    adding files

---
+++ src/server.js
@@ -0,0 +1,35 @@
+// src/server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const UserDetails = require('./models/landing');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.mongoDB)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection failed:', error);
+    });
+
+app.post('/user-details', async (req, res) => {
+    try {
+        const user = new UserDetails(req.body);
+        const savedUser = await user.save();
+        res.json(savedUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
