960246be208f4950a4b0a2fa5f2636362fc415d2
Ayan <ayandgr5@gmail.com>
2023-11-14 14:54:51 +0530

    final changes

---
+++ src/models/landing.js
@@ -0,0 +1,16 @@
+// src/models/landing.js
+
+const mongoose = require('mongoose');
+
+const landingSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: Number, required: true },
+    email: { type: String, unique: true, required: true },
+    treatment: { type: String, required: true },
+    location: { type: String, required: true }
+});
+
+
+const UserDetails = mongoose.model("SignInUser", landingSchema)
+
+module.exports = UserDetails;     
